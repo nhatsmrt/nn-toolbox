e7b94291907ce8ac3ecff5f53ed8948a6da1115f
nhatsmrt <nhatsmrt@uw.edu>
2019-06-09 17:16:02 -0700

    Display original image along transformed image.

--- nntoolbox/vision/learner/style.py
+++ nntoolbox/vision/learner/style.py
@@ -77,6 +77,7 @@ class StyleTransferLearner:
             if draw:
                 random_ind = np.random.choice(len(sample))
                 output = tensor_to_pil(self._model(sample[random_ind:random_ind+1].to(self._device)).cpu().detach())
+                display(tensor_to_pil(sample[random_ind:random_ind+1].cpu()))
                 display(output)
 
             break
