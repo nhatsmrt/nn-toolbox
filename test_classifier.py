d107abd1e90dcbb9f8ca8ca377b1b105a291c942
nhatsmrt <nhatsmrt@uw.edu>
2019-06-13 23:18:12 -0700

    Edit test.

--- test_classifier.py
+++ test_classifier.py
@@ -11,14 +11,14 @@ from nntoolbox.utils import load_model, get_device
 from sklearn.metrics import accuracy_score
 
 
-data = torchvision.datasets.CIFAR10('data/', train=True, transform=ToTensor())
+data = torchvision.datasets.CIFAR10('data/', train=True, download=True, transform=ToTensor())
 train_size = int(0.8 * len(data))
 val_size = len(data) - train_size
 train_dataset, val_dataset = torch.utils.data.random_split(data, [train_size, val_size])
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16, shuffle=True)
 
-test_dataset = torchvision.datasets.CIFAR10('data/', train=False, transform=ToTensor())
+test_dataset = torchvision.datasets.CIFAR10('data/', train=False, download=True, transform=ToTensor())
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=True)
 
 # Conv2d(1, 16, 5, stride=1, padding=2),
@@ -42,18 +42,18 @@ model = Sequential(
     Linear(in_features=120, out_features=10)
 )
 
-# criterion = CrossEntropyLoss()
-# learner = SupervisedImageLearner(
-#     train_data=train_loader,
-#     val_data=val_loader,
-#     model=model,
-#     criterion=criterion,
-#     optimizer=Adam(model.parameters()),
-#     use_scheduler=True,
-#     val_metric='accuracy'
-# )
-#
-# learner.learn(n_epoch=500, print_every=1000, save_path="weights/model.pt")
+criterion = CrossEntropyLoss()
+learner = SupervisedImageLearner(
+    train_data=train_loader,
+    val_data=val_loader,
+    model=model,
+    criterion=criterion,
+    optimizer=Adam(model.parameters()),
+    use_scheduler=True,
+    val_metric='accuracy'
+)
+
+learner.learn(n_epoch=500, print_every=1000, save_path="weights/model.pt")
 load_model(model=model, path="weights/model.pt")
 
 total = 0
