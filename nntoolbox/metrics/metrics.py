Fix a bug in double backprop (still untested). Implement label smoothing.

nntoolbox/metrics/metrics.py
@@ -1,8 +1,10 @@
 from sklearn.metrics import accuracy_score
 import torch
+from typing import Dict, Any
+
 
 class Metric:
-    def __call__(self, logs): pass
+    def __call__(self, logs: Dict[str, Any]): pass
 
     def get_best(self) -> float: return self._best
 
@@ -11,7 +13,7 @@ class Accuracy(Metric):
     def __init__(self):
         self._best = 0.0
 
-    def __call__(self, logs):
+    def __call__(self, logs: Dict[str, Any]):
         predictions = torch.argmax(logs["outputs"], dim=1).cpu().detach().numpy()
         labels = logs["labels"].cpu().numpy()
         acc = accuracy_score(
@@ -24,11 +26,12 @@ class Accuracy(Metric):
 
         return acc
 
+
 class Loss(Metric):
     def __init__(self):
         self._best = float('inf')
 
-    def __call__(self, logs):
+    def __call__(self, logs: Dict[str, Any]):
         if logs['loss'] <= self._best:
             self._best = logs['loss']
 

nntoolbox/vision/components/regularization.py
@@ -82,12 +82,13 @@ class DoubleBackpropagation(nn.Module):
     https://www.researchgate.net/profile/Harris_Drucker/publication/5576575_Improving_generalization_performance_using_double_backpropagation/links/540754510cf2c48563b2ab7f.pdf
     http://yann.lecun.com/exdb/publis/pdf/drucker-lecun-91.pdf
     '''
-    def __init__(self, model: nn.Module):
+    def __init__(self, model: nn.Module, criterion: nn.Module):
         super(DoubleBackpropagation, self).__init__()
-        self.model = model
+        self.main = model
+        self.criterion = criterion
 
-    def forward(self, input: Tensor) -> Tensor:
+    def forward(self, input: Tensor, label: Tensor) -> Tensor:
         input.requires_grad = True
-        jacobian = compute_jacobian(input, self.model, True)
+        jacobian = compute_jacobian(input, lambda input: self.criterion(self.model(input), label), True)
         input.requires_grad = False
         return 0.5 * torch.sum(jacobian * jacobian)

nntoolbox/vision/utils/data.py
@@ -1,9 +1,10 @@
 from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor
+from torch import Tensor
 from .utils import is_image
 from PIL import Image
 import os
-from typing import Tuple, Any
+from typing import Tuple, Any, Optional
 
 
 class UnlabelledImageDataset(Dataset):
@@ -28,7 +29,7 @@ class UnlabelledImageDataset(Dataset):
     def __len__(self):
         return len(self._images)
 
-    def __getitem__(self, i):
+    def __getitem__(self, i) -> Tensor:
         if self.transform is not None:
             return self._to_tensor(self.transform(self._images[i]))
         else:
@@ -39,11 +40,13 @@ class UnsupervisedFromSupervisedDataset(Dataset):
     '''
     Convert a supervisded dataset to an unsupervised dataset
     '''
-    def __init__(self, dataset: Dataset):
+    def __init__(self, dataset: Dataset, transform=None):
         self._data = dataset
+        self.transform = transform
 
     def __getitem__(self, index):
-        return self._data.__getitem__(index)[0]
+        data = self._data.__getitem__(index)[0]
+        return self.transform(data) if self.transform is not None else data
 
     def __len__(self):
         return len(self._data)