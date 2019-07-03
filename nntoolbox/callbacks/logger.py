e6644f07ab1bf9a1fcd56dfd15ff90b5edebf039
nhatsmrt <nhatsmrt@uw.edu>
2019-07-02 23:36:00 -0700

    Add options for tensorboard.

--- nntoolbox/callbacks/logger.py
+++ nntoolbox/callbacks/logger.py
@@ -4,38 +4,42 @@ from typing import Sequence
 
 
 class Tensorboard(Callback):
-    def __init__(self):
+    def __init__(self, every_iter: int=1, every_epoch: int=1):
         self._writer = SummaryWriter()
+        self._every_iter = every_iter
+        self._every_epoch = every_epoch
 
     def on_batch_end(self, logs):
-        if "loss" in logs:
-            self._writer.add_scalar(
-                tag="Training loss",
-                scalar_value=logs["loss"].item(),
-                global_step=logs["iter_cnt"]
-            )
-        if "allocated_memory" in logs:
-            self._writer.add_scalar(
-                tag="Allocated memory",
-                scalar_value=logs["allocated_memory"],
-                global_step=logs["iter_cnt"]
-            )
+        if logs["iter_cnt"] % self._every_iter == 0:
+            if "loss" in logs:
+                self._writer.add_scalar(
+                    tag="Training loss",
+                    scalar_value=logs["loss"].item(),
+                    global_step=logs["iter_cnt"]
+                )
+            if "allocated_memory" in logs:
+                self._writer.add_scalar(
+                    tag="Allocated memory",
+                    scalar_value=logs["allocated_memory"],
+                    global_step=logs["iter_cnt"]
+                )
 
     def on_epoch_end(self, logs):
-        if "epoch_metrics" in logs:
-            for metric in logs["epoch_metrics"]:
-                self._writer.add_scalar(
-                    tag= "Validation " + metric,
-                    scalar_value=logs["epoch_metrics"][metric],
-                    global_step=logs["epoch"]
-                )
-        if "draw" in logs and "tag" in logs:
-            for i in range(len(logs["tag"])):
-                self._writer.add_image(
-                    tag=logs["tag"][i],
-                    img_tensor=logs["draw"][i],
-                    global_step=logs["epoch"]
-                )
+        if logs["epoch"] % self._every_epoch == 0:
+            if "epoch_metrics" in logs:
+                for metric in logs["epoch_metrics"]:
+                    self._writer.add_scalar(
+                        tag= "Validation " + metric,
+                        scalar_value=logs["epoch_metrics"][metric],
+                        global_step=logs["epoch"]
+                    )
+            if "draw" in logs and "tag" in logs:
+                for i in range(len(logs["tag"])):
+                    self._writer.add_image(
+                        tag=logs["tag"][i],
+                        img_tensor=logs["draw"][i],
+                        global_step=logs["epoch"]
+                    )
 
 
 class LossLogger(Callback):
