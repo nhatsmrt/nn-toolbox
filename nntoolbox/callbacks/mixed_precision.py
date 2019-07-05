a011dc30580ebbaa20a003ddbe26c4d30371cc12
nhatsmrt <nhatsmrt@uw.edu>
2019-07-05 00:01:28 -0700

    Check gradient overflowing on master model.

--- nntoolbox/callbacks/mixed_precision.py
+++ nntoolbox/callbacks/mixed_precision.py
@@ -1,6 +1,7 @@
 """
 Implement mixed precision training as a callback
-Adap from fastai's course 2 v3 notebook and apex library
+Adapted from fastai's course 2 v3 notebook and apex library
+https://arxiv.org/pdf/1710.03740.pdf
 """
 import torch
 from torch.optim import Optimizer
@@ -93,7 +94,17 @@ class MixedPrecision(Callback):
         Copy the gradient to master and unscale
         :return: True if gradient does not overflow
         """
-        if self.dynamic and check_grad_overflow(self.model_param_groups):
+        to_master_grads(self.model_param_groups, self.master_param_groups)
+        for group in self.master_param_groups:
+            for param in group:
+                if param.grad is not None:
+                    # isnan_before = torch.isnan(param.grad).sum() > 0
+                    param.grad.div_(self.loss_scale)
+                    # isnan_after = torch.isnan(param.grad).sum() > 0
+                    # if (not isnan_before) and isnan_after:
+                    #     print("found problem")
+
+        if self.dynamic and check_grad_overflow(self.master_param_groups) and self.loss_scale > 1:
             # if overflow, divide the loss scale, zerograd and ignore batch:
             self.loss_scale /= self.div_factor
             print("Overflow, changing loss scale to " + str(self.loss_scale))
@@ -104,20 +115,22 @@ class MixedPrecision(Callback):
                         param.grad.zero_()
             self.count = 0
             return False
+        # if self.dynamic and check_grad_overflow(self.model_param_groups) and self.loss_scale > 1:
+        #     # if overflow, divide the loss scale, zerograd and ignore batch:
+        #     self.loss_scale /= self.div_factor
+        #     print("Overflow, changing loss scale to " + str(self.loss_scale))
+        #     # self.learner._model.zero_grad()
+        #     for group in self.model_param_groups:
+        #         for param in group:
+        #             if param.requires_grad:
+        #                 param.grad.zero_()
+        #     self.count = 0
+        #     return False
 
-        to_master_grads(self.model_param_groups, self.master_param_groups)
-        for group in self.master_param_groups:
-            for param in group:
-                if param.grad is not None:
-                    isnan_before = torch.isnan(param.grad).sum() > 0
-                    param.grad.div_(self.loss_scale)
-                    isnan_after = torch.isnan(param.grad).sum() > 0
-                    if (not isnan_before) and isnan_after:
-                        print("found problem")
 
         if self.dynamic:
             self.count += 1
-            if self.count == self.scale_wait:
+            if self.count >= self.scale_wait and self.loss_scale < self.max_loss_scale:
                 self.count = 0
                 self.loss_scale *= self.div_factor
                 print("Increasing loss scale to " + str(self.loss_scale))
