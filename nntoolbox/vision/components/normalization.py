8d46c152661452b9ee80c0cc849438a4935986fc
nhatsmrt <nhatsmrt@uw.edu>
2019-07-06 06:10:53 -0700

    Implement self stabilization layer.

--- nntoolbox/vision/components/normalization.py
+++ nntoolbox/vision/components/normalization.py
@@ -2,6 +2,11 @@ import torch
 from torch import nn, Tensor
 from torch.nn import functional as F
 from typing import Tuple
+import numpy as np
+
+
+__all__ = ['L2NormalizationLayer', 'AdaIN', 'SelfStabilizer']
+
 
 class L2NormalizationLayer(nn.Module):
     def __init__(self):
@@ -12,9 +17,9 @@ class L2NormalizationLayer(nn.Module):
 
 
 class AdaIN(nn.Module):
-    '''
+    """
     Implement adaptive instance normalization layer
-    '''
+    """
     def __init__(self):
         super(AdaIN, self).__init__()
         self._style = None
@@ -33,11 +38,28 @@ class AdaIN(nn.Module):
 
     @staticmethod
     def compute_mean_std(images: Tensor) -> Tuple[Tensor, Tensor]:
-        '''
+        """
         :param images: (n_img, C, H, W)
         :return: (n_img, C, H, W)
-        '''
+        """
         images_reshaped = images.view(images.shape[0], images.shape[1], -1)
         images_mean = images_reshaped.mean(2).unsqueeze(-1).unsqueeze(-1)
         images_std = images_reshaped.std(2).unsqueeze(-1).unsqueeze(-1)
         return images_mean, images_std
+
+
+class SelfStabilizer(nn.Module):
+    """
+    Self stabilize layer, based on:
+    https://www.cntk.ai/pythondocs/cntk.layers.blocks.html
+    https://www.cntk.ai/pythondocs/_modules/cntk/layers/blocks.html#Stabilizer
+    https://www.cntk.ai/pythondocs/layerref.html#batchnormalization-layernormalization-stabilizer
+    https://www.microsoft.com/en-us/research/wp-content/uploads/2016/11/SelfLR.pdf
+    """
+    def __init__(self, steepness: float=4):
+        super(SelfStabilizer, self).__init__()
+        self.steepness = steepness
+        self.param = nn.Parameter(torch.tensor(np.log(np.exp(steepness) - 1) / steepness))
+
+    def forward(self, input: Tensor) -> Tensor:
+        return F.softplus(self.param, beta=self.steepness) * input
