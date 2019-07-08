9b73a3de348333d2ab4e7c3034536ac98f5265e9
nhatsmrt <nhatsmrt@uw.edu>
2019-07-07 18:50:11 -0700

    Add debug printings

--- nntoolbox/callbacks/progress_bar.py
+++ nntoolbox/callbacks/progress_bar.py
@@ -16,12 +16,11 @@ class ProgressBar(Callback):
     n_epoch: int
 
     def on_train_begin(self):
+        print("num epoch: " + str(self.n_epoch))
         self.master_bar = master_bar(range(self.n_epoch))
         self.master_bar.on_iter_begin()
         self.set_progress(self.learner._train_data, 0)
         print("on train begin")
-
-    def on_train_end(self): self.master_bar.on_iter_end()
 
     def on_batch_end(self, logs: Dict[str, Any]):
         print("On batch end")
@@ -33,7 +32,9 @@ class ProgressBar(Callback):
         self.set_progress(self.learner._train_data, logs["epoch"] + 1)
         return False
 
+    def on_train_end(self): self.master_bar.on_iter_end()
+
     def set_progress(self, data: DataLoader, epoch: int):
-        print("Epoch :" + str(self.n_epoch))
+        print("Epoch: " + str(epoch))
         self.progress_bar = progress_bar(data, parent=self.master_bar, auto_update=False)
         self.master_bar.update(epoch)
