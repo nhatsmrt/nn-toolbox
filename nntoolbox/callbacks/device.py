53daa09346f5f5106815adfc1657ef0f8d0d1173
nhatsmrt <nhatsmrt@uw.edu>
2019-07-10 02:12:29 -0700

    Add a callback to transfer data for gpipe

--- nntoolbox/callbacks/device.py
+++ nntoolbox/callbacks/device.py
@@ -4,7 +4,7 @@ from torch import Tensor
 from ..utils import get_device
 
 
-__all__ = ['ToDeviceCallback']
+__all__ = ['ToDeviceCallback', 'DataToGPipeDeviceCallback']
 
 
 class ToDeviceCallback(Callback):
@@ -12,10 +12,27 @@ class ToDeviceCallback(Callback):
         self._device = device
         self.learner = None
 
-    # def on_train_begin(self):
-    #     self.learner._model = self.learner._model.to(self._device)
+    def on_train_begin(self):
+        self.learner._model = self.learner._model.to(self._device)
 
     def on_batch_begin(self, data: Dict[str, Tensor], train: bool) -> Dict[str, Tensor]:
         for key in data:
             data[key] = data[key].to(self._device)
+        return data
+
+
+class DataToGPipeDeviceCallback(Callback):
+    def __init__(self, input_keys, target_keys):
+        self.learner = None
+        self.input_keys = input_keys
+        self.target_keys = target_keys
+
+    def on_batch_begin(self, data: Dict[str, Tensor], train: bool) -> Dict[str, Tensor]:
+        in_device = self.learner._model.devices[0]
+        out_device = self.learner._model.devices[-1]
+        for key in data:
+            if key in self.input_keys:
+                data[key] = data[key].to(in_device, non_blocking=True)
+            elif key in self.target_keys:
+                data[key] = data[key].to(out_device, non_blocking=True)
         return data
