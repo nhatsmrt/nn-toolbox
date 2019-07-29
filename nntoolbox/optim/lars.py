4d603268d87f6318ad88f7ebdc8541a52a4fe7cd
nhatsmrt <nhatsmrt@uw.edu>
2019-07-29 04:32:21 -0700

    Add a trust coefficient.

--- nntoolbox/optim/lars.py
+++ nntoolbox/optim/lars.py
@@ -15,10 +15,11 @@ class LARS(SGD):
         https://arxiv.org/pdf/1708.03888.pdf
     """
 
-    def __init__(self, params, lr: float, momentum: float=0.0, weight_decay: float=0.0):
+    def __init__(self, params, lr: float,  momentum: float=0.0, weight_decay: float=0.0, trust_coefficient: float=0.02):
         super(LARS, self).__init__(
             params, lr, momentum, 0.0, weight_decay, False
         )
+        self.trust_coefficient = trust_coefficient
 
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -42,8 +43,10 @@ class LARS(SGD):
 
                 data_norm = p.data.norm(2)
                 grad_norm = p.grad.data.norm(2)
-                local_lr = (data_norm / (grad_norm + weight_decay * data_norm)).detach()
+                print(grad_norm)
+                local_lr = (self.trust_coefficient * data_norm / (grad_norm + weight_decay * data_norm)).detach()
                 print(local_lr)
+                print()
 
                 if weight_decay != 0:
                     d_p.add_(weight_decay, p.data)
