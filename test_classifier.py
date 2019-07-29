7638ec6fc1a5baf9e8ad771c329d91e0399822fe
nhatsmrt <nhatsmrt@uw.edu>
2019-07-29 01:49:32 -0700

    Change batch size for training.

--- test_classifier.py
+++ test_classifier.py
@@ -73,9 +73,11 @@ train_dataset.dataset.transform = Compose(
         ToTensor()
     ]
 )
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=128, shuffle=False)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1024, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=512, shuffle=False)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False)
+
+print("Number of batches per epoch " + str(len(train_loader)))
 
 
 class SEResNeXtShakeShake(ResNeXtBlock):
