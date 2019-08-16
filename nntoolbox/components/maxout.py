0dafaa7bc613984ddbba946219e2f863e8f554af
nhatsmrt <nhatsmrt@uw.edu>
2019-08-15 21:48:59 -0700

    Implement a module for combining multiple losses.

--- nntoolbox/components/maxout.py
+++ nntoolbox/components/maxout.py
@@ -3,11 +3,14 @@ from torch import nn, Tensor
 
 
 class MaxoutLinear(nn.Module):
-    '''
-    A linear maxout layer
-    https://arxiv.org/pdf/1302.4389.pdf
-    output_i = max_{j = 1,...,k} (w_1 input + b_1, w_2 input + b_2,..., w_k input + b_k)
-    '''
+    """
+    A linear maxout layer:
+
+        output_i = max_{j = 1,...,k} (w_1 input + b_1, w_2 input + b_2,..., w_k input + b_k)
+
+    References:
+        Ian J. Goodfellow et al. "Maxout Networks." https://arxiv.org/pdf/1302.4389.pdf
+    """
     def __init__(self, in_features: int, out_features: int, nb_features: int, bias: bool=True):
         super(MaxoutLinear, self).__init__()
         self._features = nn.ModuleList(
@@ -15,9 +18,9 @@ class MaxoutLinear(nn.Module):
         )
 
     def forward(self, input: Tensor) -> Tensor:
-        '''
+        """
         :param input: (batch_size, in_features)
         :return: (batch_size, out_features)
-        '''
+        """
         features = [self._features[i](input) for i in range(len(self._features))]
         return torch.max(torch.stack(features, dim=-1), dim=-1)[0]
--- nntoolbox/losses/losses.py
+++ nntoolbox/losses/losses.py
@@ -1,9 +1,10 @@
 from torch.nn import MSELoss
-from torch import Tensor
+from torch import Tensor, nn
 import torch
+from typing import List, Optional
 
 
-__all__ = ['RMSELoss']
+__all__ = ['RMSELoss', 'CombinedLoss']
 
 
 class RMSELoss(MSELoss):
@@ -13,5 +14,19 @@ class RMSELoss(MSELoss):
 
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
         return torch.sqrt(super().forward(input, target) + self._eps)
+
+
+class CombinedLoss(nn.Module):
+    def __init__(self, losses: List[nn.Module], weights: Optional[List[float]]=None):
+        super(CombinedLoss, self).__init__()
+        if weights is not None: assert len(weights) == len(losses)
+        else: weights = [1.0 / len(losses) for _ in range(len(losses))]
+        self.losses = nn.ModuleList(losses)
+        self.weights = weights
+
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        losses = torch.stack([self.losses[i](input, target) * self.weights[i] for i in range(len(self.losses))], -1)
+        return torch.sum(losses)
+
 
 
