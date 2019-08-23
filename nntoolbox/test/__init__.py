cd56da42bc15e5f6fad52616bf22937f62ea6d7c
nhatsmrt <nhatsmrt@uw.edu>
2019-08-23 03:28:40 -0700

    Add description for test.

--- nntoolbox/test/__init__.py
+++ nntoolbox/test/__init__.py
@@ -1,3 +1,4 @@
+"""Utilities for testing machine learning programs and some example tests"""
 from .prog_bar import progress_bar_test
 from .test_classifier import run_classifier_test
 from .test import *
