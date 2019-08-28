ef46e0d219e9d185b5d07eaf73e7afce0018b1f1
nhatsmrt <nhatsmrt@uw.edu>
2019-08-27 19:16:51 -0700

    Add blueprint for developing rnn components.

--- nntoolbox/sequence/components/rnn.py
+++ nntoolbox/sequence/components/rnn.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn, Tensor
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, PackedSequence
 from typing import Tuple, Optional, Callable, Union, List
 import torch.jit as jit
 from collections import OrderedDict
@@ -110,39 +109,3 @@ class JitLSTMLayer(JitRNNLayer):
             outputs.append(output)
 
         return torch.stack(outputs, dim=0), state
-
-
-# INCOMPLETE
-class ReverseRNN(nn.Module):
-    """
-    Explicitly encode RNN for reverse direction (INCOMPLETE)
-    """
-    def __init__(self, base_rnn, num_layers: int, input_size: int, hidden_size: int, dropout: float = 0.0, **kwargs):
-        super(ReverseRNN, self).__init__()
-        self.base_rnn = base_rnn(
-            num_layers=1, input_size=input_size, hidden_size=hidden_size,
-            bidirectional=False, **kwargs
-        )
-
-    def forward(self, input, h_0, lengths: None):
-        if isinstance(input, PackedSequence):
-            input_unpacked, lengths = pad_packed_sequence(input)
-            input_packed = input
-        elif isinstance(input, Tensor):
-            assert lengths is not None
-            assert len(input.shape) == 3
-
-            input_unpacked = input
-            input_packed = pack_padded_sequence(input, lengths=lengths)
-        else:
-            raise ValueError
-
-        if h_0 is None:
-            h_0 = torch.zeros(
-                size=(self._num_layers, input_unpacked.shape[1], input_unpacked.shape[2])
-            ).to(input_unpacked.device)
-
-        output, h_n = self.base_rnn(input_packed, h_0)
-        return
-
-
