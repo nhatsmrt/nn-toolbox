cc6f5628dc459357db6d630b2d7d4449b63f75c0
nhatsmrt <nhatsmrt@uw.edu>
2019-08-27 20:07:09 -0700

    Implement another variant of ResidualRNN.

--- nntoolbox/sequence/components/rnn.py
+++ nntoolbox/sequence/components/rnn.py
@@ -6,9 +6,58 @@ from collections import OrderedDict
 
 
 __all__ = [
-    'RNNDropout', 'RNNSequential', 'JitRNNDropout', 'JitRNNSequential',
-    'JitRNNLayer', 'JitLSTMLayer'
+    'JitRNNLayer', 'JitLSTMLayer', 'RNNDropout',
+    'RNNSequential', 'JitRNNDropout', 'JitRNNSequential',
+    'JitResidualRNNV2', 'JitLSTMSequential', 'JitResidualLSTMV2'
 ]
+
+
+class JitRNNLayer(jit.ScriptModule):
+    """
+    Implement an RNN layer with jit script module
+
+    References:
+
+        The PyTorch Team. "Optimizing CUDA Recurrent Neural Networks with TorchScript."
+        https://pytorch.org/blog/optimizing-cuda-rnn-with-torchscript/
+    """
+    def __init__(self, base_cell: Callable[..., Union[jit.ScriptModule, nn.Module]], *cell_args):
+        super().__init__()
+        self.base_cell = base_cell(*cell_args)
+
+    @jit.script_method
+    def forward(self, input: Tensor, state: Optional[Tensor]=None) -> Tuple[Tensor, Optional[Tensor]]:
+        inputs = input.unbind(0)
+        outputs = jit.annotate(List[Tensor], [])
+
+        for t in range(len(inputs)):
+            state = self.base_cell(inputs[t], state)
+            outputs.append(state)
+
+        return torch.stack(outputs, dim=0), state
+
+
+class JitLSTMLayer(JitRNNLayer):
+    """
+    Implement an LSTM layer with jit script module
+
+    References:
+
+        The PyTorch Team. "Optimizing CUDA Recurrent Neural Networks with TorchScript."
+        https://pytorch.org/blog/optimizing-cuda-rnn-with-torchscript/
+    """
+    @jit.script_method
+    def forward(
+            self, input: Tensor, state: Optional[Tuple[Tensor, Tensor]]=None
+    ) -> Tuple[Tensor, Optional[Tuple[Tensor, Tensor]]]:
+        inputs = input.unbind(0)
+        outputs = jit.annotate(List[Tensor], [])
+
+        for t in range(len(inputs)):
+            output, state = self.base_cell(inputs[t], state)
+            outputs.append(output)
+
+        return torch.stack(outputs, dim=0), state
 
 
 class RNNDropout(nn.Module):
@@ -63,49 +112,80 @@ class JitRNNSequential(jit.ScriptModule):
         return input, h_0
 
 
-class JitRNNLayer(jit.ScriptModule):
+class JitResidualRNNV2(JitRNNSequential):
     """
-    Implement an RNN layer with jit script module
-
-    References:
-
-        The PyTorch Team. "Optimizing CUDA Recurrent Neural Networks with TorchScript."
-        https://pytorch.org/blog/optimizing-cuda-rnn-with-torchscript/
+    Implement a simple residual stacked RNN
     """
-    def __init__(self, base_cell: Callable[..., Union[jit.ScriptModule, nn.Module]], *cell_args):
-        super().__init__()
-        self.base_cell = base_cell(*cell_args)
+    __constants__ = ['_modules_list', 'skip_length']
+
+    def __init__(self, layers: List[Union[jit.ScriptModule, nn.Module]], skip_length: int=1):
+        """
+        :param layers: rnn layers. Must have output dimension the same as input dimension
+        :param skip_length: length of the skip
+        """
+        super(JitResidualRNNV2, self).__init__(layers)
+        assert skip_length > 0
+        self.skip_length = skip_length
 
     @jit.script_method
-    def forward(self, input: Tensor, state: Optional[Tensor]=None) -> Tuple[Tensor, Optional[Tensor]]:
-        inputs = input.unbind(0)
-        outputs = jit.annotate(List[Tensor], [])
+    def forward(self, input: Tensor, h_0: Optional[Tensor]=None) -> Tuple[Tensor, Optional[Tensor]]:
+        output = jit.annotate(Tensor, torch.zeros(1))
+        l = 1
+        for module in self._modules_list:
+            output, h_0 = module(input, h_0)
+            if l % self.skip_length == 0:
+                input = output + input
+            else:
+                input = output
+            l += 1
+        return output, h_0
 
-        for t in range(len(inputs)):
-            state = self.base_cell(inputs[t], state)
-            outputs.append(state)
 
-        return torch.stack(outputs, dim=0), state
-
-
-class JitLSTMLayer(JitRNNLayer):
+class JitLSTMSequential(jit.ScriptModule):
     """
-    Implement an LSTM layer with jit script module
-
-    References:
-
-        The PyTorch Team. "Optimizing CUDA Recurrent Neural Networks with TorchScript."
-        https://pytorch.org/blog/optimizing-cuda-rnn-with-torchscript/
+    Implement a simple stacked LSTM
     """
+    __constants__ = ['_modules_list']
+
+    def __init__(self, layers: List[Union[jit.ScriptModule, nn.Module]]):
+        super(JitLSTMSequential, self).__init__()
+        self._modules_list = nn.ModuleList(layers)
+
     @jit.script_method
     def forward(
-            self, input: Tensor, state: Optional[Tuple[Tensor, Tensor]]=None
+            self, input: Tensor, states: Optional[Tuple[Tensor, Tensor]]=None
     ) -> Tuple[Tensor, Optional[Tuple[Tensor, Tensor]]]:
-        inputs = input.unbind(0)
-        outputs = jit.annotate(List[Tensor], [])
+        for module in self._modules_list:
+            input, states = module(input, states)
+        return input, states
 
-        for t in range(len(inputs)):
-            output, state = self.base_cell(inputs[t], state)
-            outputs.append(output)
 
-        return torch.stack(outputs, dim=0), state
+class JitResidualLSTMV2(JitLSTMSequential):
+    """
+    Implement a simple residual stacked RNN
+    """
+    __constants__ = ['_modules_list', 'skip_length']
+
+    def __init__(self, layers: List[Union[jit.ScriptModule, nn.Module]], skip_length: int=1):
+        """
+        :param layers: rnn layers. Must have output dimension the same as input dimension
+        :param skip_length: length of the skip
+        """
+        super().__init__(layers)
+        assert skip_length > 0
+        self.skip_length = skip_length
+
+    @jit.script_method
+    def forward(
+            self, input: Tensor, states: Optional[Tuple[Tensor, Tensor]]=None
+    ) -> Tuple[Tensor, Optional[Tuple[Tensor, Tensor]]]:
+        output = jit.annotate(Tensor, torch.zeros(1))
+        l = 1
+        for module in self._modules_list:
+            output, states = module(input, states)
+            if l % self.skip_length == 0:
+                input = output + input
+            else:
+                input = output
+            l += 1
+        return output, states
