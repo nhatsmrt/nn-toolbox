b4231b9838a48602f37c1e4132899923989cddb1
nhatsmrt <nhatsmrt@uw.edu>
2019-09-04 05:41:52 -0700

    Reimplement DNDFTree with BFS and DP ideas.

--- nntoolbox/components/dndf.py
+++ nntoolbox/components/dndf.py
@@ -2,8 +2,7 @@
 from functools import partial
 import torch
 from torch import nn, Tensor
-from .components import ModifyByLambda
-from .merge import Multiply, Mean
+from .merge import Mean
 
 
 __all__ = ['DNDFTree', 'DNDF']
@@ -12,7 +11,7 @@ __all__ = ['DNDFTree', 'DNDF']
 class DNDFTree(nn.Module):
     """
     Based on Deep Neural Decision Forest, but with the leaf node parameterized for end-to-end training, and the decision
-    trees balanced.
+    trees balanced. Use BFS + DP for fast path computations
 
     References:
 
@@ -23,37 +22,29 @@ class DNDFTree(nn.Module):
             self, in_features: int, out_features: int, tree_depth: int,
             output_activation=partial(nn.Softmax, dim=1)
     ):
-        assert tree_depth > 1 and in_features > 1
         super().__init__()
-        n_leaves = 2 ** tree_depth
-
-        self.leaves = nn.ModuleList(
-            [nn.Sequential(nn.Linear(in_features, out_features), output_activation(dim=1)) for _ in range(n_leaves)]
-        )
-        decision_functions = nn.ModuleList(
-            [nn.Sequential(nn.Linear(in_features, 1), nn.Sigmoid()) for _ in range(n_leaves - 1)]
-        )
-
-        routings = []
-        for l in range(n_leaves):
-            route = []
-
-            node_ind = 0
-            index = l
-            for d in range(tree_depth):
-                if index % 2 == 0:
-                    route.append(decision_functions[node_ind])
-                else:
-                    route.append(ModifyByLambda(decision_functions[node_ind], lambda t: 1.0 - t))
-                node_ind = node_ind * 2 + 1 + index % 2
-                index = index >> 1
-            routings.append(Multiply(route))
-        self.routings = nn.ModuleList(routings)
+        self.n_leaves = 2 ** tree_depth
+        self.tree_depth = tree_depth
+        self.out_features = out_features
+        self.output_activation = output_activation()
+        self.transform = nn.Linear(in_features, out_features * self.n_leaves + self.n_leaves - 1)
 
     def forward(self, input: Tensor) -> Tensor:
-        routings = [route(input) for route in self.routings]
-        leaves = [leaf(input) for leaf in self.leaves]
-        return (torch.stack(routings, dim=-1) * torch.stack(leaves, dim=-1)).sum(dim=-1)
+        features = self.transform(input)
+        decision_nodes, leaves = torch.sigmoid(features[:, :self.n_leaves - 1]), \
+                                 features[:, :self.out_features * self.n_leaves]
+        neg_decision_nodes = 1.0 - decision_nodes
+        leaves = self.output_activation(leaves.view(-1, self.out_features, self.n_leaves)).permute(2, 0, 1)
+        routings = [1.0]
+
+        for d in range(self.tree_depth):
+            new_level = []
+            for f in range(2 ** d - 1, 2 ** (d + 1) - 1):
+                new_level.append(routings[f - 2 ** d + 1] * decision_nodes[:, f:f + 1])
+                new_level.append(routings[f - 2 ** d + 1] * neg_decision_nodes[:, f:f + 1])
+            routings = new_level
+
+        return torch.stack([routings[i] * leaves[i] for i in range(self.n_leaves)], dim=-1).sum(-1)
 
 
 class DNDF(Mean):
