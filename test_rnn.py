7d1e956ccc8baf2eef8195d13716d0ad8b16a6c2
nhatsmrt <nhatsmrt@uw.edu>
2019-09-05 19:01:31 -0700

    Update README.

--- test_rnn.py
+++ test_rnn.py
@@ -8,7 +8,7 @@ from nntoolbox.sequence.components import \
     JitRNNLayer, JitRNNSequential, JitResidualRNNV2, ONLSTMCell, ONLSTMCellV2, \
     JitLSTMSequential, JitResidualLSTMV2
 from nntoolbox.hooks import OutputHook
-from nntoolbox.components import DropConnect, JitDropConnect
+from nntoolbox.components import DropConnect
 # from nntoolbox.components import LWTA
 
 # print(MILSTMCell(input_size=128, hidden_size=256))
@@ -28,25 +28,27 @@ from nntoolbox.components import DropConnect, JitDropConnect
 
 
 # cell = MultiplicativeRNNCell(input_size=128, hidden_size=256, intermediate_size=64)
-cell = DropConnect(RecurrentHighwayCell(128, 256, 1), weight_names=["weight_s"], ps=[0.5])
-# cell = ONLSTMCell(128, 256)
-inp = torch.rand(32, 128)
-h0 = torch.zeros(32, 256)
-# print(cell(inp, h0).shape)
-target = torch.rand(32, 256)
-optimizer = torch.optim.Adam(cell.parameters())
-loss = nn.MSELoss()
+# DropConnectFast = lambda *args, **kwargs: jit.script(JitDropConnect(*args, **kwargs))
+# cell = JitDropConnect(RecurrentHighwayCell(128, 256, 1), [0.5], ["weight_s"])
+# # cell = ONLSTMCell(128, 256)
+# inp = torch.rand(32, 128)
+# h0 = torch.zeros(32, 256)
+# # print(cell(inp, h0).shape)
+# target = torch.rand(32, 256)
+# optimizer = torch.optim.Adam(cell.parameters())
+# loss = nn.MSELoss()
+#
+#
+# for _ in range(1000):
+#     optimizer.zero_grad()
+#     l = loss(cell(inp, h0), target)
+#     l.backward()
+#     optimizer.step()
+#     print(l)
 
 
-for _ in range(1000):
-    optimizer.zero_grad()
-    l = loss(cell(inp), target)
-    l.backward()
-    optimizer.step()
-    print(l)
-
-# layer = JitRNNLayer(RecurrentHighwayCell, 128, 256, 3)
-# layer = JitLSTMLayer(MILSTMCell, 128, 256)
+# layer = JitRNNLayer(RecurrentHighwayCell, 128, 256, 3, recurrent_drop_p=0.1, inp_drop_p=0.1)
+layer = JitLSTMLayer(MILSTMCell, 128, 256, recurrent_drop_p=0.1, inp_drop_p=0.1)
 # layer = JitRNNLayer(MIGRUCell, 128, 256)
 # layer = JitRNNSequential(
 #     layers=[JitRNNLayer(MIGRUCell, 128, 256)] +
@@ -62,20 +64,20 @@ for _ in range(1000):
 
 # layer = nn.Sequential(layer)
 # hook = OutputHook(layer)
-# inp = torch.rand(7, 32, 128)
-# # state = torch.zeros((inp.shape[1], 256)).to(inp.device).to(inp.dtype)
-# target = torch.rand(7, 32, 256)
-# optimizer = torch.optim.Adam(layer.parameters())
-# loss = nn.MSELoss()
-# #
-# for _ in range(10000):
-#     optimizer.zero_grad()
-#     l = loss(layer(inp)[0], target)
-#     l.backward()
-#     # print(next(layer.parameters()).grad)
-#     optimizer.step()
-#     print(l)
+inp = torch.rand(7, 32, 128)
+# state = torch.zeros((inp.shape[1], 256)).to(inp.device).to(inp.dtype)
+target = torch.rand(7, 32, 256)
+optimizer = torch.optim.Adam(layer.parameters())
+loss = nn.MSELoss()
+#
+for _ in range(10000):
+    optimizer.zero_grad()
+    l = loss(layer(inp)[0], target)
+    l.backward()
+    # print(next(layer.parameters()).grad)
+    optimizer.step()
+    print(l)
 
-# print(layer(inp)[0].shape)
-#
-#
+print(layer(inp)[0].shape)
+
+
