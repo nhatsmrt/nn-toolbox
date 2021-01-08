9080a2048d95a4e12a053820b5f1a7d4298de4cf
nhatsmrt <nphamcs@gmail.com>
2021-01-08 15:59:46 -0800

    Add setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open('README_PYPI.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as req_file:
+    requirements = [line[:-1] for line in req_file if len(line) > 1]
+
+
+setuptools.setup(
+    name="nn-toolbox",
+    version="0.1.0",
+    author="Nhat Pham",
+    author_email="nphamcs@gmail.com",
+    description="A toolbox for common deep learning procedures",
+    long_description=long_description,
+    url="https://github.com/nhatsmrt/nn-toolbox",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
