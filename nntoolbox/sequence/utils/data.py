86bbb8a88f1a873ff7a9f9cd0727c3e514335f66
Nhat Pham <nphamcs@gmail.com>
2021-01-10 16:02:02 -0800

    Add dependency on dill and remove dependency on spacy (#20)
    * Add dill to requirements
    
    * Remove spacy model default
    
    * Make spacy dependency optional
    
    * Add extra requirements file
    
    * Add another fn to __all__
    
    * Change error message
    
    * Remove albumentations from requirements extra

--- nntoolbox/sequence/utils/data.py
+++ nntoolbox/sequence/utils/data.py
@@ -1,11 +1,9 @@
+from warnings import warn
 from torchtext.vocab import Vocab
-import spacy
 from typing import List
-import multiprocessing as mp
-from functools import partial
 
 
-__all__ = ['id_to_text', 'text_to_id', 'tokenize_sentence']
+__all__ = ['id_to_text', 'text_to_id']
 
 
 def id_to_text(sequence, vocab: Vocab) -> List[str]:
@@ -30,20 +28,32 @@ def text_to_id(sequence: List[str], vocab: Vocab) -> List[int]:
     return [vocab.stoi[word] for word in sequence]
 
 
-def tokenize_sentence(sentence: str, model=spacy.load('en')) -> List[str]:
-    """Tokenize a single sentence"""
-    return model.tokenizer(sentence)
+try:
+    import spacy
+    import multiprocessing as mp
+    from functools import partial
 
 
-def tokenize_fn(ind: int, sentences: List[str], model=spacy.load('en')):
-  tokenized = tokenize_sentence(sentences[ind], model)
-  return tokenized
+    __all__ += ['tokenize_sentence', 'tokenize_sentences']
 
 
-def tokenize_sentences(sentences: List[str], model=spacy.load('en'), num_workers: int=1) -> List[List[str]]:
-    """Tokenize a list of sentences"""
-    if num_workers > 1:
-        pool = mp.Pool(processes=num_workers)
-        return list(pool.map(partial(tokenize_fn, sentences, model), range(len(sentences))))
 
-    return [tokenize_sentence(sentence, model) for sentence in sentences]
+    def tokenize_sentence(sentence: str, model) -> List[str]:
+        """Tokenize a single sentence"""
+        return model.tokenizer(sentence)
+
+
+    def tokenize_fn(ind: int, sentences: List[str], model):
+      tokenized = tokenize_sentence(sentences[ind], model)
+      return tokenized
+
+
+    def tokenize_sentences(sentences: List[str], model, num_workers: int=1) -> List[List[str]]:
+        """Tokenize a list of sentences"""
+        if num_workers > 1:
+            pool = mp.Pool(processes=num_workers)
+            return list(pool.map(partial(tokenize_fn, sentences, model), range(len(sentences))))
+
+        return [tokenize_sentence(sentence, model) for sentence in sentences]
+except ImportError:
+    warn("spacy is not installed so tokenizing methods in nntoolbox.sequence.utils.data will not be available.")
