7879ae7c25a550180fe1151c0946819f33dbee31
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 13:28:17 +0000

    Adding tarfile member sanitization to extractall()

--- nntoolbox/sequence/utils/datasets.py
+++ nntoolbox/sequence/utils/datasets.py
@@ -35,7 +35,26 @@ class Europarl(TranslationDataset):
             tar_path = root + non_en + "-" + "en.tgz"
             download_from_url(url, tar_path, max_size=None)
             with tarfile.open(tar_path) as tar:
-                tar.extractall(root)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, root)
         else:
             print("Data already downloaded.")
 
